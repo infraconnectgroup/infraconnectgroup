d2fb9fe5de1dd35a41fc1c3fcfd8363a005e9b2f
bcislah <bcislah@gmail.com>
2026-05-18 14:55:36 +0200

    logo van 12 naar 16

--- src/components/site/Header.tsx
+++ src/components/site/Header.tsx
@@ -26,7 +26,7 @@ export function Header() {
           <img
             src={logo}
             alt="Al Islah logo"
-            className="h-12 w-12 object-contain"
+            className="h-16 w-16 object-contain"
           />
 
           <div className="flex flex-col leading-tight">
