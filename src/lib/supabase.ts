668ea85ce4335aa8656039e2602842b3be09ef5f
infraconnectgroup <infraconnectgroup@gmail.com>
2026-06-03 08:57:24 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://mzgobfulqqabznqflhjq.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_uL2hLYBKeK3JIAs0wbXcXQ_dzcF78Bh";
+const SUPABASE_URL = "https://zktbvkyvpkteqfsbqthl.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Mdl87fzU_ok3Cv96_97uaA_K9a6-gAv";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
